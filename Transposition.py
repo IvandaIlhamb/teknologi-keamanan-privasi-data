1214c4bcc707976311df12096c29dde8939c3dbc
IvandaIlham <l200200199@student.ums.ac.id>
2023-03-22 07:17:52 +0700

    First commit

--- Transposition.py
+++ Transposition.py
@@ -5,14 +5,14 @@ plain_text = input("PLAIN TEXT: ").lower().replace(" ", "")
 len_key = len(key)
 len_plain = len(plain_text)
 row = int(math.ceil(len_plain / len_key))
-matrix = [ ['X']*len_key for i in range(row) ]
+matriks = [ ['X']*len_key for i in range(row) ]
 
 
 
 t = 0
 for r in range(row):
   for c,ch in enumerate(plain_text[t : t+ len_key]):
-    matrix[r][c] = ch
+    matriks[r][c] = ch
   t += len_key
 
 #Membuat urutan abjad
@@ -22,8 +22,8 @@ sort_order = sorted([(ch,i) for i,ch in enumerate(key)])
 cipher_text = ''
 for ch,c in sort_order:
   for r in range(row):
-    cipher_text += matrix[r][c]
-
+    cipher_text += matriks[r][c]
+  
 print("Encryption")
-print("Plain Text :",plain_text)
-print("Cipher Text :",cipher_text)
+print("Plain text :",plain_text)
+print("Cipher text :",cipher_text)
